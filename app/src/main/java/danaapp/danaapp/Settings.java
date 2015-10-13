7e766615d6cb2140a8048a8e72d0b146c6b54c19
Lorelai L <lorelai.jane@gmail.com>
2015-10-14 00:24:20 +0200

    initial commit

---
+++ app/src/main/java/danaapp/danaapp/Settings.java
@@ -0,0 +1,8 @@
+package danaapp.danaapp;
+
+
+public class Settings {
+    public static final String DEV_NAME = "<DEV_NAME_HERE>";
+    public static short insSensitivity = 36;
+    public static double durationOfInsActionInHours = 5;
+}
